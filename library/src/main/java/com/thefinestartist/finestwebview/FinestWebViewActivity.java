63f17b784eccff838d2fd082a067470c272b1d0f
gr8woo <gr8wooya@gmail.com>
2018-06-14 16:20:12 +0900

    fix image upload in webview

--- library/src/main/java/com/thefinestartist/finestwebview/FinestWebViewActivity.java
+++ library/src/main/java/com/thefinestartist/finestwebview/FinestWebViewActivity.java
@@ -221,6 +221,13 @@ public class FinestWebViewActivity extends AppCompatActivity
   protected LinearLayout menuOpenWith;
   protected TextView menuOpenWithTv;
   protected FrameLayout webLayout;
+
+
+  private ValueCallback<Uri> filePathCallbackNormal;
+  private ValueCallback<Uri[]> filePathCallbackLollipop;
+  private final static int FILECHOOSER_NORMAL_REQ_CODE = 1;
+  private final static int FILECHOOSER_LOLLIPOP_REQ_CODE = 2;
+
   DownloadListener downloadListener = new DownloadListener() {
     @Override public void onDownloadStart(String url, String userAgent, String contentDisposition,
         String mimetype, long contentLength) {
@@ -1130,6 +1137,20 @@ public class FinestWebViewActivity extends AppCompatActivity
     }, ViewConfiguration.getZoomControlsTimeout() + 1000L);
   }
 
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (requestCode == FILECHOOSER_NORMAL_REQ_CODE) {
+      if (filePathCallbackNormal == null) return ;
+      Uri result = (data == null || resultCode != RESULT_OK) ? null : data.getData();
+      filePathCallbackNormal.onReceiveValue(result);
+      filePathCallbackNormal = null;
+    } else if (requestCode == FILECHOOSER_LOLLIPOP_REQ_CODE) {
+      if (filePathCallbackLollipop == null) return ;
+      filePathCallbackLollipop.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, data));
+      filePathCallbackLollipop = null;
+    }
+  }
+
   public class MyWebChromeClient extends WebChromeClient {
 
     @Override public void onProgressChanged(WebView view, int progress) {
@@ -1163,6 +1184,74 @@ public class FinestWebViewActivity extends AppCompatActivity
 
     @Override public void onReceivedTouchIconUrl(WebView view, String url, boolean precomposed) {
       BroadCastManager.onReceivedTouchIconUrl(FinestWebViewActivity.this, key, url, precomposed);
+    }
+
+    public void openFileChooser( ValueCallback<Uri> uploadMsg) {
+      Log.d("Android Version: ", "3.0 <");
+      openFileChooser(uploadMsg, "");
+    }
+    // For Android 3.0+
+    public void openFileChooser( ValueCallback<Uri> uploadMsg, String acceptType) {
+      Log.d("Android Version: ", "3.0+");
+      filePathCallbackNormal = uploadMsg;
+      Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+      i.addCategory(Intent.CATEGORY_OPENABLE);
+      i.setType("image/*");
+      startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_NORMAL_REQ_CODE);
+    }
+    // For Android 4.1+
+    public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+      Log.d("Android Version: ", "4.1+");
+      openFileChooser(uploadMsg, acceptType);
+    }
+
+    // For Android 5.0+
+    public boolean onShowFileChooser(
+            WebView webView, ValueCallback<Uri[]> filePathCallback,
+            WebChromeClient.FileChooserParams fileChooserParams) {
+      Log.d("Android Version: ", "5.0+");
+      if (filePathCallbackLollipop != null) {
+        filePathCallbackLollipop.onReceiveValue(null);
+        filePathCallbackLollipop = null;
+      }
+      filePathCallbackLollipop = filePathCallback;
+      Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+      i.addCategory(Intent.CATEGORY_OPENABLE);
+      i.setType("image/*");
+      startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_LOLLIPOP_REQ_CODE);
+
+      return true;
+    }
+
+
+    @Override
+    public boolean onCreateWindow(WebView view, boolean dialog, boolean userGesture, Message resultMsg) {
+
+      newWebView = new WebView(view.getContext());
+//                    WebSettings webSettings = newWebView.getSettings();
+//                    webSettings.setJavaScriptEnabled(true);
+//                    webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+//                    webSettings.setSupportMultipleWindows(true);
+//
+//                    newWebView.setWebViewClient(new MyWebViewClient());
+//
+//
+//
+//
+//                    swipeRefreshLayout.addView(newWebView);    // 화면에 보여질 수 있도록 add view
+
+      WebView.WebViewTransport transport = (WebView.WebViewTransport)resultMsg.obj;
+      transport.setWebView(newWebView);
+      resultMsg.sendToTarget();
+
+      return true;
+    }
+
+    @Override
+    public void onCloseWindow(WebView window) {
+      super.onCloseWindow(window);
+//                    webLayout.removeView(window);    // 화면에서 제거
+//                    swipeRefreshLayout.removeView(window);
     }
   }
 
